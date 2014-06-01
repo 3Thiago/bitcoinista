58a9baa31df52946f62a3738af354911f639042a
christianlundkvist <christian.lundkvist@gmail.com>
2014-06-01 10:08:44 -0400

    Fix reclimit bug.

--- bitcoinista.py
+++ bitcoinista.py
@@ -1,13 +1,18 @@
 import sys
 import bitcoinista
 
+# Supported user modes are:
+# 'mainnet'
+# 'demo'
+
 user_mode = 'mainnet'
+
 if __name__ == '__main__':
     # Pythonista resets recursion 
     # limit to 256 in the interpreter
     # so we need to force it here
     reclimit = sys.getrecursionlimit()
-    if reclimit > 1000:
+    if reclimit < 1000:
         sys.setrecursionlimit(1000)
     ctrl = bitcoinista.TextController(user_mode)
     try:
